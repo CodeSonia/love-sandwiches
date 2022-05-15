1c257edc3b6e9f6edd16ee89d20c5973e88bf3e5
CodeSonia <sonia_ah@icloud.com>
2022-05-15 18:17:13 +0000

    Create get_sales data function to get data string from user

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-# Imports the entire gspread library so we can access every class and function within it
-import gspread 
+# Imports the entire gspread library so we can access every
+# class and function within it
+import gspread
 
-# Imports the Credentials class which is part of the service account function in the google auth libray
+# Imports the Credentials class which is part of the
+# service account function in the google auth libray
 from google.oauth2.service_account import Credentials
 
 # All CAPS variable = constant variable
@@ -9,7 +11,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 # Created settings to access data from the google sheet
 CREDS = Credentials.from_service_account_file('creds.json')
@@ -17,8 +19,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-print(data)
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+
+get_sales_data()
+
